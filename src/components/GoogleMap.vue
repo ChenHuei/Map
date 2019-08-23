37369a670f47b1b2d6afc37ddc389fabfbcafc6a
Huei <aaa830714@gmail.com>
2019-08-23 17:12:31 +0800

    first commit

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div>
+      <h2>Search and add a pin</h2>
+      <label>
+        <gmap-autocomplete
+          @place_changed="setPlace">
+        </gmap-autocomplete>
+        <button @click="addMarker">Add</button>
+      </label>
+      <br/>
+    </div>
+    <br>
+    <gmap-map
+      :center="center"
+      :zoom="12"
+      style="width:100%; height: 400px"
+    >
+      <gmap-marker
+        v-for="(m, index) in markers"
+        :key="index"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleMap',
+  data () {
+    return {
+      center: { lat: 39.0017355, lng: 141.10259599999995 },
+      markers: [],
+      currentPlace: null
+    }
+  },
+
+  mounted () {
+    this.geolocate()
+  },
+
+  methods: {
+    setPlace (place) {
+      this.currentPlace = place
+    },
+    addMarker () {
+      if (this.currentPlace) {
+        const marker = {
+          lat: this.currentPlace.geometry.location.lat(),
+          lng: this.currentPlace.geometry.location.lng()
+        }
+        this.markers.push({ position: marker })
+        this.center = marker
+        this.currentPlace = null
+      }
+    },
+    geolocate () {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      })
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDuheGUHe_rH1snoJEklSw1uX754-Gch9k',
+    libraries: 'places'
+  }
+})
 
 new Vue({
   router,
